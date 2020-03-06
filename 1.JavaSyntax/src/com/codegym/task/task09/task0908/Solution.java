8bd36f863fb1e2bc5f8fbd331fde80b66518950e
SHTUNDER <shtunder.andrey1yandex.by>
2020-03-07 00:27:58 +0300

    task0908

---
+++ 1.JavaSyntax/src/com/codegym/task/task09/task0908/Solution.java
@@ -0,0 +1,21 @@
+package com.codegym.task.task09.task0908;
+
+/* 
+Exception while working with strings
+
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //write your code here
+        try {
+            String s = null;
+            String m = s.toLowerCase();
+        }
+        catch(NullPointerException e){
+            System.out.println("NullPointerException has been caught");
+        }
+
+        //write your code here
+    }
+}
