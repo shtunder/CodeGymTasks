fe0dd0b5ebc605eebc39886be4dae5d552a52733
SHTUNDER <shtunder.andrey1yandex.by>
2020-03-07 23:24:58 +0300

    task0909

---
+++ 1.JavaSyntax/src/com/codegym/task/task09/task0909/Solution.java
@@ -0,0 +1,21 @@
+package com.codegym.task.task09.task0909;
+
+/* 
+Exception when working with arrays
+
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //write your code here
+        try {
+            int[] m = new int[2];
+            m[8] = 5;
+        }
+        catch(IndexOutOfBoundsException e){
+            System.out.println("IndexOutOfBoundsException has been caught");
+        }
+
+        //write your code here
+    }
+}
