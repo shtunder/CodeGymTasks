b7e236563060260e3b0ecb210905362461ba0ade
SHTUNDER <shtunder.andrey1yandex.by>
2020-03-11 19:54:22 +0300

    task0915

---
+++ 1.JavaSyntax/src/com/codegym/task/task09/task0915/Solution.java
@@ -0,0 +1,52 @@
+package com.codegym.task.task09.task0915;
+
+import java.io.CharConversionException;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystemException;
+
+/* 
+Catching custom exceptions
+
+*/
+
+public class Solution {
+    public static StatelessBean BEAN = new StatelessBean();
+
+    public static void main(String[] args) {
+        try {
+            handleExceptions();
+        } catch (FileSystemException e){
+            BEAN.log(e);
+        }
+    }
+
+    public static void handleExceptions() throws FileSystemException {
+        try {
+            BEAN.throwExceptions();
+        } catch (FileSystemException e){
+            BEAN.log(e);
+            throw e;
+        } catch (CharConversionException e){
+            BEAN.log(e);
+        } catch (IOException e){
+            BEAN.log(e);
+        }
+    }
+
+    public static class StatelessBean {
+        public void log(Exception exception) {
+            System.out.println(exception.getMessage() + ", " + exception.getClass().getSimpleName());
+        }
+
+        public void throwExceptions() throws CharConversionException, FileSystemException, IOException {
+            int i = (int) (Math.random() * 3);
+            if (i == 0)
+                throw new CharConversionException();
+            if (i == 1)
+                throw new FileSystemException("");
+            if (i == 2)
+                throw new IOException();
+        }
+    }
+}
