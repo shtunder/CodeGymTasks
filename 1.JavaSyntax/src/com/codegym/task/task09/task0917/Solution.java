28225500934496f66e02a1647644e512175753a3
SHTUNDER <shtunder.andrey1yandex.by>
2020-03-20 00:23:03 +0300

    task0917

---
+++ 1.JavaSyntax/src/com/codegym/task/task09/task0917/Solution.java
@@ -0,0 +1,47 @@
+package com.codegym.task.task09.task0917;
+
+/* 
+Catching unchecked exceptions
+
+*/
+
+
+public class Solution {
+    public static void main(String[] args) {
+        handleExceptions(new Solution());
+    }
+
+    public static void handleExceptions(Solution obj) {
+        try {
+            obj.method1();
+            obj.method2();
+            obj.method3();
+        } catch(NumberFormatException e){
+            printStack(e);
+        } catch(NullPointerException e){
+            printStack(e);
+        } catch(IndexOutOfBoundsException e){
+            printStack(e);
+        }
+    }
+
+    public static void printStack(Throwable throwable) {
+        System.out.println(throwable);
+        for (StackTraceElement element : throwable.getStackTrace()) {
+            System.out.println(element);
+        }
+    }
+
+    public void method1() {
+        throw new NullPointerException();
+    }
+
+    public void method2() {
+        throw new IndexOutOfBoundsException();
+    }
+
+    public void method3() {
+        throw new NumberFormatException();
+    }
+}
+
