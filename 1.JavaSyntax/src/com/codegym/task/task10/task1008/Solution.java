b4531b796222a226dd590aaac0177bec85bf71b6
SHTUNDER <shtunder.andrey1yandex.by>
2020-04-02 22:45:51 +0300

    task1008

---
+++ 1.JavaSyntax/src/com/codegym/task/task10/task1008/Solution.java
@@ -0,0 +1,15 @@
+package com.codegym.task.task10.task1008;
+
+/* 
+Correct answer: d = 2.941
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = 50;
+        int b = 17;
+        double d = (double) a / b;
+        System.out.println(d);
+    }
+}
