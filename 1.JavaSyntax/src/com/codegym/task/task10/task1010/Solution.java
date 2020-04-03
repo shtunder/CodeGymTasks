35259e569f2e8cccd20dc0b653df9bf28c0d0e05
SHTUNDER <shtunder.andrey1yandex.by>
2020-04-03 16:17:49 +0300

    tasks 1010-1013

---
+++ 1.JavaSyntax/src/com/codegym/task/task10/task1010/Solution.java
@@ -0,0 +1,18 @@
+package com.codegym.task.task10.task1010;
+
+/* 
+Correct answer: d = 1.0
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = 257;
+        int b = 4;
+        int c = 3;
+        int e = 2;
+        double d = (byte) a + b / c / e;
+
+        System.out.println(d);
+    }
+}
