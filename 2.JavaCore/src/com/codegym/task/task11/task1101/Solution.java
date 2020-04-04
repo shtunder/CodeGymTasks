18b659aceee95915388d1f50ff108457c85f2369
SHTUNDER <shtunder.andrey1yandex.by>
2020-04-05 00:07:29 +0300

    task1101

---
+++ 2.JavaCore/src/com/codegym/task/task11/task1101/Solution.java
@@ -0,0 +1,19 @@
+package com.codegym.task.task11.task1101;
+
+/* 
+Horse and Pegasus
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Horse {
+
+    }
+
+    public class Pegasus extends Horse {
+
+    }
+}
