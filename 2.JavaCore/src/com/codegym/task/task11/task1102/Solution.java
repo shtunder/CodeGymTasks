6cdb2a353074834409d264a1ba8303d0453b654d
SHTUNDER <shtunder.andrey1yandex.by>
2020-04-06 22:46:05 +0300

    task1102

---
+++ 2.JavaCore/src/com/codegym/task/task11/task1102/Solution.java
@@ -0,0 +1,23 @@
+package com.codegym.task.task11.task1102;
+
+/* 
+Pets
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Pet {
+
+    }
+
+    public class Cat extends Pet {
+
+    }
+
+    public class Dog extends Pet {
+
+    }
+}
