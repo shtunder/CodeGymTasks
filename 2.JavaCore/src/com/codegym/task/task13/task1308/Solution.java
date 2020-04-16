cb29835476ed3b1bc380c6d6e3d273869588e113
SHTUNDER <shtunder.andrey1yandex.by>
2020-04-17 00:03:34 +0300

    tasks 1307 - 1310

---
+++ 2.JavaCore/src/com/codegym/task/task13/task1308/Solution.java
@@ -0,0 +1,19 @@
+package com.codegym.task.task13.task1308;
+
+/* 
+Hey, are you alive?
+
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+
+    public interface Person {
+        boolean isAlive();
+    }
+
+    public interface Presentable extends Person {
+
+    }
+}
---
+++ 2.JavaCore/src/com/codegym/task/task13/task1310/Solution.java
@@ -0,0 +1,48 @@
+package com.codegym.task.task13.task1310;
+
+/* 
+Who's that smarty?
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    interface Person {
+        void use(Person person);
+
+        void startToWork();
+    }
+
+    interface HasManagementPotential {
+        boolean inspiresOthersToWork();
+    }
+
+    interface Secretary extends Person {
+    }
+
+    interface Boss extends Person, HasManagementPotential {
+    }
+
+    class Manager implements Boss {
+        public void use(Person person) {
+            person.startToWork();
+        }
+
+        public void startToWork() {
+        }
+
+        public boolean inspiresOthersToWork() {
+            return true;
+        }
+    }
+
+    class Subordinate implements Secretary {
+        public void use(Person person) {
+        }
+
+        public void startToWork() {
+        }
+    }
+}
