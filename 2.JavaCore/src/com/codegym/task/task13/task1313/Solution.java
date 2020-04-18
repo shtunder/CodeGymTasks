4659ab6e4ef6d5cac58b4ef466df236d8ea77fd1
SHTUNDER <shtunder.andrey1yandex.by>
2020-04-18 22:13:37 +0300

    tasks 1312 - 1314

---
+++ 2.JavaCore/src/com/codegym/task/task13/task1313/Solution.java
@@ -0,0 +1,23 @@
+package com.codegym.task.task13.task1313;
+
+import java.awt.*;
+
+/* 
+A fox is an animal
+
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+
+    public interface Animal {
+        Color getColor();
+    }
+
+    public abstract static class Fox implements Animal {
+        public String getName() {
+            return "Fox";
+        }
+    }
+}
