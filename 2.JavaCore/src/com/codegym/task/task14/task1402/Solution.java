8465d66b12d84381f01c803e6f67eaee1253ecb3
SHTUNDER <shtunder.andrey1yandex.by>
2020-05-02 13:53:07 +0300

    tasks 1401 - 1402

---
+++ 2.JavaCore/src/com/codegym/task/task14/task1402/Solution.java
@@ -0,0 +1,28 @@
+package com.codegym.task.task14.task1402;
+
+/* 
+Bingo!
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Cat cat = new TomCat();
+
+        boolean isCat = cat instanceof Cat;
+        boolean isMovable = cat instanceof CanMove;
+        boolean isTom = cat instanceof TomCat;
+
+        if (isCat && isMovable && isTom) System.out.println("Bingo!");
+    }
+
+    interface CanMove {
+    }
+
+    static class Cat implements CanMove {
+    }
+
+    static class TomCat extends Cat {
+
+    }
+}
