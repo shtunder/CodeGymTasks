c47bcbcc04785b3ae7f008bcfae0c3eb96511338
SHTUNDER <shtunder.andrey1yandex.by>
2020-05-04 19:58:44 +0300

    task 1408

---
+++ 2.JavaCore/src/com/codegym/task/task14/task1409/Bridge.java
@@ -0,0 +1,5 @@
+package com.codegym.task.task14.task1409;
+
+public interface Bridge {
+    int getCarsCount();
+}
---
+++ 2.JavaCore/src/com/codegym/task/task14/task1409/Solution.java
@@ -0,0 +1,19 @@
+package com.codegym.task.task14.task1409;
+
+/* 
+Bridges
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        println(new WaterBridge());
+        println(new SuspensionBridge());
+    }
+
+    // Add println method here
+    public static void println(Bridge bridge){
+        System.out.println(bridge.getCarsCount());
+    }
+}
+
