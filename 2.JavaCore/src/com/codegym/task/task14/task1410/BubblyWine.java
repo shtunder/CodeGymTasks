a3902c822595f4c4ad4db5538cc732d3fe03ffe5
SHTUNDER <shtunder.andrey1yandex.by>
2020-05-04 20:16:43 +0300

    task 1410

---
+++ 2.JavaCore/src/com/codegym/task/task14/task1410/BubblyWine.java
@@ -0,0 +1,8 @@
+package com.codegym.task.task14.task1410;
+
+public class BubblyWine extends Wine {
+    @Override
+    public String getCelebrationName() {
+        return "New Year's";
+    }
+}
---
+++ 2.JavaCore/src/com/codegym/task/task14/task1410/Solution.java
@@ -0,0 +1,27 @@
+package com.codegym.task.task14.task1410;
+
+/* 
+Wine tasting
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        getDeliciousDrink().taste();
+        System.out.println(getWine().getCelebrationName());
+        System.out.println(getBubblyWine().getCelebrationName());
+        System.out.println(getWine().getCelebrationName());
+    }
+
+    public static Drink getDeliciousDrink() {
+        return new Wine();
+    }
+
+    public static Wine getWine() {
+        return new Wine();
+    }
+
+    public static Wine getBubblyWine() {
+        return new BubblyWine();
+    }
+}
