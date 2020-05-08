0556a9d3ab7fa4c84e16131d39db38e4ef6f2d5f
SHTUNDER <shtunder.andrey1yandex.by>
2020-05-08 23:41:11 +0300

    task 1501

---
+++ 2.JavaCore/src/com/codegym/task/task15/task1501/Solution.java
@@ -0,0 +1,41 @@
+package com.codegym.task.task15.task1501;
+
+/* 
+OOP: Arrange interfaces
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public interface Movable {
+        boolean isMovable();
+    }
+
+    public interface Sellable {
+        Object getAllowedAction(String name);
+    }
+
+    public interface Discountable {
+        Object getAllowedAction();
+    }
+
+    public static class Clothes implements Movable, Sellable, Discountable {
+        @Override
+        public boolean isMovable() {
+            return false;
+        }
+
+        @Override
+        public Object getAllowedAction(String name) {
+            return null;
+        }
+
+        @Override
+        public Object getAllowedAction() {
+            return null;
+        }
+    }
+}
