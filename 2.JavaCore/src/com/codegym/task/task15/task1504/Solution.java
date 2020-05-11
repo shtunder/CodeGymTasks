f6ea090141a40ae413e9ffa0126af6ee57c1044b
SHTUNDER <shtunder.andrey1yandex.by>
2020-05-11 23:48:40 +0300

    task 1504

---
+++ 2.JavaCore/src/com/codegym/task/task15/task1504/Solution.java
@@ -0,0 +1,89 @@
+package com.codegym.task.task15.task1504;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/* 
+OOP: Books
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        List<Book> books = new LinkedList<Book>();
+        books.add(new MarkTwainBook("Tom Sawyer"));
+        books.add(new AgathaChristieBook("Hercule Poirot"));
+        System.out.println(books);
+    }
+
+    abstract static class Book {
+        private String author;
+
+        public Book(String author) {
+            this.author = author;
+        }
+
+        public abstract Book getBook();
+
+        public abstract String getTitle();
+
+        private String getOutputByBookType() {
+            String agathaChristieOutput = author + ", " + getBook().getTitle() + " is a detective";
+            String markTwainOutput = getBook().getTitle() + " book was written by " + author;
+
+            String output = "output";
+            //Add your code here
+            if (getBook() instanceof AgathaChristieBook)
+                output = agathaChristieOutput;
+
+            if (getBook() instanceof MarkTwainBook)
+                output = markTwainOutput;
+
+            return output;
+        }
+
+        public String toString() {
+            return getOutputByBookType();
+        }
+    }
+
+    public static class MarkTwainBook extends Book {
+        private String title;
+
+        public MarkTwainBook(String title) {
+            super("Mark Twain");
+            this.title = title;
+        }
+
+        @Override
+        public MarkTwainBook getBook() {
+            return this;
+        }
+
+        @Override
+        public String getTitle() {
+            return this.title;
+        }
+    }
+
+    public static class AgathaChristieBook extends Book {
+        private String title;
+
+        public AgathaChristieBook(String title) {
+            super("Agatha Christie");
+            this.title = title;
+        }
+
+        @Override
+        public AgathaChristieBook getBook() {
+            return this;
+        }
+
+        @Override
+        public String getTitle() {
+            return this.title;
+        }
+    }
+
+
+}
