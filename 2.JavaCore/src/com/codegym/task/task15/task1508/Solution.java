d2d8972070912aa38dead9cb70fef833455a73f0
SHTUNDER <shtunder.andrey1yandex.by>
2020-05-18 23:20:19 +0300

    task 1508

---
+++ 2.JavaCore/src/com/codegym/task/task15/task1508/Solution.java
@@ -0,0 +1,17 @@
+package com.codegym.task.task15.task1508;
+
+/* 
+OOP: Method overloading - Eliminating the superfluous
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        print(1);
+    }
+
+    public static void print(long l) {
+        System.out.println("I will be a Java programmer!");
+    }
+
+}
