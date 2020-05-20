cb9a573dfe41a2def47ce54385a76fd176ea01ca
SHTUNDER <shtunder.andrey1yandex.by>
2020-05-20 20:55:46 +0300

    task 1511

---
+++ 2.JavaCore/src/com/codegym/task/task15/task1511/Solution.java
@@ -0,0 +1,38 @@
+package com.codegym.task.task15.task1511;
+
+import java.io.Serializable;
+
+/* 
+The simplest code: part 1
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        JuniorJavaDev me = new JuniorJavaDev();
+        System.out.println(me.askHubert("What do you think about com.codegym.task.task15.task1511?"));
+        System.out.println(me.askZapp("When will the next update happen?"));
+    }
+
+    public interface SpecificSerializable extends Serializable {
+    }
+
+    public static class JavaDev implements SpecificSerializable {
+        String answerQuestion(String question) {
+            return String.format("I'll be thinking of [%s]", question);
+        }
+    }
+
+    public static class JuniorJavaDev extends JavaDev implements SpecificSerializable {
+        JavaDev zapp = new JavaDev();
+        JavaDev hubert = new JavaDev();
+
+        String askZapp(String question) {
+            return zapp.answerQuestion(question);
+        }
+
+        String askHubert(String question) {
+            return hubert.answerQuestion(question);
+        }
+    }
+}
