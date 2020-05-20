2e31376bb8dd83c32747571311510022ad2ade69
SHTUNDER <shtunder.andrey1yandex.by>
2020-05-20 21:43:12 +0300

    task 1513

---
+++ 2.JavaCore/src/com/codegym/task/task15/task1513/Solution.java
@@ -0,0 +1,21 @@
+package com.codegym.task.task15.task1513;
+
+/* 
+The simplest code: part 3
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public interface Runnable {
+    }
+
+    public class Machine implements Runnable {
+    }
+
+    public class Car extends Machine {
+    }
+
+}
