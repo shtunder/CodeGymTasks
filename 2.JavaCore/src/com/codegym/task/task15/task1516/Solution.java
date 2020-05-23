c9f971ea70ab255ac009fc5e7649046a1453936b
SHTUNDER <shtunder.andrey1yandex.by>
2020-05-23 15:38:51 +0300

    task 1516

---
+++ 2.JavaCore/src/com/codegym/task/task15/task1516/Solution.java
@@ -0,0 +1,26 @@
+package com.codegym.task.task15.task1516;
+
+/* 
+Default values
+
+*/
+
+public class Solution {
+    public static int intVar;
+    public static double doubleVar;
+    public static Double DoubleVar;
+    public static boolean booleanVar;
+    public static Object ObjectVar;
+    public static Exception ExceptionVar;
+    public static String StringVar;
+    
+    public static void main(String[] args) {
+        System.out.println(intVar);
+        System.out.println(doubleVar);
+        System.out.println(DoubleVar);
+        System.out.println(booleanVar);
+        System.out.println(ObjectVar);
+        System.out.println(ExceptionVar);
+        System.out.println(StringVar);
+    }
+}
