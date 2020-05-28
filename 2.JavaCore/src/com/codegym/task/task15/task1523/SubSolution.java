2269b5c5b25118b7658ba4bd59694ad1a3f7c96e
SHTUNDER <shtunder.andrey1yandex.by>
2020-05-28 23:49:00 +0300

    task 1523

---
+++ 2.JavaCore/src/com/codegym/task/task15/task1523/SubSolution.java
@@ -0,0 +1,14 @@
+package com.codegym.task.task15.task1523;
+
+public class SubSolution extends Solution {
+    public SubSolution() {
+    }
+
+    protected SubSolution(int i) {
+        super(i);
+    }
+
+    SubSolution(double d) {
+        super(d);
+    }
+}
