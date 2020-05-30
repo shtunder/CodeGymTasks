0c938477b83ebd7c9c9476408a764632776e04e3
SHTUNDER <shtunder.andrey1yandex.by>
2020-05-30 22:58:02 +0300

    task 1524

---
+++ 2.JavaCore/src/com/codegym/task/task15/task1524/Solution.java
@@ -0,0 +1,45 @@
+package com.codegym.task.task15.task1524;
+
+/* 
+Order of loading variables
+
+*/
+
+public class Solution {
+    static {
+        init();
+        System.out.println("Static block");
+    }
+
+    {
+        System.out.println("non-static block");
+        printAllFields(this);
+    }
+
+    public int i = 6;
+
+    public String name = "First name";
+//    static {
+//
+//    }
+
+    public Solution() {
+        System.out.println("Solution constructor");
+        printAllFields(this);
+    }
+
+    public static void init() {
+        System.out.println("static void init()");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("public static void main");
+        Solution s = new Solution();
+    }
+
+    public static void printAllFields(Solution obj) {
+        System.out.println("static void printAllFields");
+        System.out.println(obj.i);
+        System.out.println(obj.name);
+    }
+}
