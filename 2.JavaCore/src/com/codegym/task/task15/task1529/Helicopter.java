4af6bcf96ddec571bd51c221c43de12a94efac17
SHTUNDER <shtunder.andrey1yandex.by>
2020-06-03 23:55:13 +0300

    task 1529

---
+++ 2.JavaCore/src/com/codegym/task/task15/task1529/Helicopter.java
@@ -0,0 +1,8 @@
+package com.codegym.task.task15.task1529;
+
+public class Helicopter implements CanFly {
+    @Override
+    public void fly() {
+
+    }
+}
---
+++ 2.JavaCore/src/com/codegym/task/task15/task1529/Solution.java
@@ -0,0 +1,41 @@
+package com.codegym.task.task15.task1529;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Mastering the static block
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    static {
+        //add your code here - добавьте код тут
+        Solution.reset();
+    }
+
+    public static CanFly result;
+
+    public static void reset() {
+        //add your code here - добавьте код тут
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            String s = reader.readLine();
+            if (s.equals("helicopter"))
+                result = new Helicopter();
+            if (s.equals("plane")) {
+                int n = Integer.parseInt(reader.readLine());
+                result = new Plane(n);
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
