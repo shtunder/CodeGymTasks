1b722792daaf74eda649168ec836e0fda185ba3f
SHTUNDER <shtunder.andrey1yandex.by>
2020-06-04 22:03:55 +0300

    task 1530

---
+++ 2.JavaCore/src/com/codegym/task/task15/task1530/DrinkMaker.java
@@ -0,0 +1,12 @@
+package com.codegym.task.task15.task1530;
+
+public abstract class DrinkMaker {
+    public abstract void getRightCup();
+    public abstract void putIngredient();
+    public abstract void pour();
+    public void makeDrink() {
+        getRightCup();
+        putIngredient();
+        pour();
+    }
+}
