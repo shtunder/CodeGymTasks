6f77beb89ac1b1bb412f45d921d148910994340b
SHTUNDER <shtunder.andrey1yandex.by>
2020-06-10 22:19:00 +0300

    task 1602

---
+++ 2.JavaCore/src/com/codegym/task/task16/task1602/Solution.java
@@ -0,0 +1,25 @@
+package com.codegym.task.task16.task1602;
+
+/* 
+My second thread
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        TestThread thread = new TestThread();
+        thread.start();
+    }
+
+    public static class TestThread extends Thread {
+
+        static {
+            System.out.println("it's a static block inside TestThread");
+        }
+
+        public void run() {
+            System.out.println("it's a run method");
+
+        }
+    }
+}
