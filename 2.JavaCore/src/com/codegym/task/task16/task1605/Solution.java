16d681c58e347e3a4407c4c487a3b997647d0cb8
SHTUNDER <shtunder.andrey1yandex.by>
2020-06-13 23:14:32 +0300

    task 1605

---
+++ 2.JavaCore/src/com/codegym/task/task16/task1605/Solution.java
@@ -0,0 +1,58 @@
+package com.codegym.task.task16.task1605;
+
+import java.util.Date;
+
+/* 
+Let's talk music
+
+*/
+
+public class Solution {
+    public static int delay = 1000;
+
+    public static void main(String[] args) {
+        Thread violin = new Thread(new Violin("Player"));
+        violin.start();
+    }
+
+    public static void sleepNSeconds(int n) {
+        try {
+            Thread.sleep(n * delay);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public interface MusicalInstrument extends Runnable {
+        Date startPlaying();
+
+        Date stopPlaying();
+    }
+
+    public static class Violin implements MusicalInstrument {
+        private String owner;
+
+        public Violin(String owner) {
+            this.owner = owner;
+        }
+
+        public Date startPlaying() {
+            System.out.println(this.owner + " starts playing");
+            return new Date();
+        }
+
+        public Date stopPlaying() {
+            System.out.println(this.owner + " stops playing");
+            return new Date();
+        }
+
+        @Override
+        public void run() {
+            Date start = startPlaying();
+            sleepNSeconds(1);
+            Date finish = stopPlaying();
+
+            System.out.println("Playing " + (finish.getTime() - start.getTime()) + " ms");
+        }
+    }
+}
