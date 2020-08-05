5f1bc65654ab361a973b19943f1409fef75c002e
Andrey <shtunder.andrey@gmail.com>
2020-08-05 20:39:05 +0300

    task 1701

---
+++ 2.JavaCore/src/com/codegym/task/task17/task1701/Solution.java
@@ -0,0 +1,45 @@
+package com.codegym.task.task17.task1701;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Notes
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        new NoteThread().start();
+        new NoteThread().start();
+    }
+
+    public static class Note {
+
+        public static final List<String> notes = new ArrayList<String>();
+
+        public static void addNote(String note) {
+            notes.add(0, note);
+        }
+
+        public static void removeNote(String threadName) {
+            String note = notes.remove(0);
+            if (note == null) {
+                System.out.println("Другая нить удалила нашу заметку");
+            } else if (!note.startsWith(threadName)) {
+                System.out.println("Нить [" + threadName + "] удалила чужую заметку [" + note + "]");
+            }
+        }
+    }
+
+    public static class NoteThread extends Thread {
+        @Override
+        public void run() {
+            for (int i=0; i<1000; i++) {
+                Note.addNote(getName() + "-Note" + i);
+                Note.removeNote(getName());
+                //comments: You have black screen? For use Thread.sleep here.
+            }
+        }
+    }
+}
