7ebabb5353b4fe73991e34671d5bd76cfa0b9bea
SHTUNDER <shtunder.andrey1yandex.by>
2020-09-02 23:54:44 +0300

    task 1711

---
+++ 2.JavaCore/src/com/codegym/task/task17/task1711/Solution.java
@@ -0,0 +1,133 @@
+package com.codegym.task.task17.task1711;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+/* 
+CRUD 2
+
+*/
+
+public class Solution {
+    public static volatile List<Person> allPeople = new ArrayList<Person>();
+
+    static {
+        allPeople.add(Person.createMale("Иванов Иван", new Date()));  //сегодня родился    id=0
+        allPeople.add(Person.createMale("Петров Петр", new Date()));  //сегодня родился    id=1
+    }
+
+    public static void main(String[] args) {
+        //start here - начни тут
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH);
+        DateFormat dateFormatPrt = new SimpleDateFormat("dd-MMM-yyyy", Locale.ENGLISH);
+
+        switch (args[0]) {
+
+            //create person
+            case "-c":
+                for (int step = 1; step < args.length; step += 3) {
+                    Date date = null;
+                    try {
+                        date = dateFormat.parse(args[step + 2]);
+                    } catch (ParseException e) {
+                        e.getMessage();
+                    }
+
+                    //VALIDATOR: При запуске программы с параметром -с программа должна добавлять человека с заданными
+                    // параметрами в конец списка allPeople, и выводить id (index) на экран.
+                    Person person;
+                    if (args[step + 1].startsWith("м"))
+                        person = Person.createMale(args[step], date);
+                    else
+                        person = Person.createFemale(args[step], date);
+
+                    synchronized (allPeople) {
+                        allPeople.add(person);
+                    }
+
+                    System.out.println(allPeople.indexOf(person));
+                }
+                break;
+
+            //update person
+            case "-u": {
+                synchronized (allPeople) {
+                    for (int step = 1; step < args.length; step += 4) {
+                        int index = Integer.parseInt(args[step]);
+                        Person person = allPeople.get(index);
+
+                        //update name
+                        person.setName(args[step + 1]);
+
+                        //udate date
+                        Date date = null;
+                        try {
+                            date = dateFormat.parse(args[step + 3]);
+                        } catch (ParseException e) {
+                            e.getMessage();
+                        }
+                        person.setBirthDay(date);
+
+                        //update sex
+                        if (args[step + 2].startsWith("м"))
+                            person.setSex(Sex.MALE);
+                        else
+                            person.setSex(Sex.FEMALE);
+                    }
+                } //synchronized
+                break;
+            }
+
+            //remove person
+            case "-d": {
+                synchronized (allPeople) {
+                    for (int i = 1; i < args.length; i++) {
+                        int index = Integer.parseInt(args[i]);
+                        Person person = allPeople.get(index);
+
+                        person.setName(null);
+                        person.setSex(null);
+                        person.setBirthDay(null);
+                    }
+                }
+                break;
+            }
+
+            //print person
+            case "-i": {
+                synchronized (allPeople) {
+                    for (int i = 1; i < args.length; i++) {
+                        int index = Integer.parseInt(args[i]);
+                        Person person = allPeople.get(index);
+                        StringBuffer s = new StringBuffer();
+                        s.append(person.getName());
+                        s.append(" ");
+                        s.append(person.getSex() == Sex.MALE ? "м" : "ж");
+                        s.append(" ");
+                        s.append(dateFormatPrt.format(person.getBirthDay()));
+                        System.out.println(s);
+                    }
+                }
+            }
+            break;
+        }
+
+        //For DEBUG
+        /*for (Person person : allPeople) {
+            StringBuffer s = new StringBuffer();
+            s.append(person.getName());
+            s.append(" ");
+            s.append(person.getSex() == Sex.MALE ? "м" : "ж");
+            s.append(" ");
+            if (person.getBirthDay()!=null)
+                s.append(dateFormatPrt.format(person.getBirthDay()));
+            System.out.println(s);
+        }*/
+
+    }
+}
