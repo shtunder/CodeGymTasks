4ddee311a54ff145efcaf87514bfbcd638fc7eca
SHTUNDER <shtunder.andrey1yandex.by>
2020-11-02 23:57:08 +0300

    task 1806

--- 2.JavaCore/src/com/codegym/task/task18/task1806/Solution.java
+++ 2.JavaCore/src/com/codegym/task/task18/task1806/Solution.java
@@ -11,18 +11,18 @@ Fix the bugs
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        FileInputStream inputStream = new FileInputStream("d:/data.txt");
-        // Создаем поток-записи-байт-в-файл
-        FileOutputStream outputStream = new FileOutputStream("d:/result.txt");
+        FileInputStream inputStream = new FileInputStream("c:/data.txt");
+        // Create a stream to write bytes to a file
+        FileOutputStream outputStream = new FileOutputStream("c:/result.txt");
 
-        if (inputStream.available() > 0) {
-            //читаем весь файл одним куском
-            byte[] buffer = new byte[inputStream.available()];
+        if (inputStream.available() >= 0) {
+            // Read the entire file in one batch
+            byte[] buffer = new byte[inputStream.read()];
             int count = inputStream.read(buffer);
             outputStream.write(buffer, 0, count);
         }
 
-        inputStream.close();
-        outputStream.close();
+        inputStream.reset();
+        outputStream.flush();
     }
 }
