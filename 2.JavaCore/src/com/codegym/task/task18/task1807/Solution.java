4b7908bb791776060aa0e9964b3e7928b889bdd8
SHTUNDER <shtunder.andrey1yandex.by>
2020-11-04 00:47:44 +0300

    task 1807

--- 2.JavaCore/src/com/codegym/task/task18/task1807/Solution.java
+++ 2.JavaCore/src/com/codegym/task/task18/task1807/Solution.java
@@ -1,38 +1,55 @@
 package com.codegym.task.task18.task1807;
 
-/* 
-Counting commas
-
-*/
-
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/* 
+Counting commas
+
+*/
+
 public class Solution {
     public static void main(String[] args) {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//        String fileName = reader.readLine();
+//        int commaCount = 0;
+//        try (FileInputStream inputStream = new FileInputStream(fileName)) {
+//            while (inputStream.available() > 0) {
+//                if (inputStream.read() == 44)
+//                    commaCount++;
+//            }
+//            System.out.println(commaCount);
+//        }
+        FileInputStream inputStream = null;
+        byte[] buffer = null;
+
         try {
             String fileName = reader.readLine();
-            FileInputStream inputStream = new FileInputStream("D:/test.txt");
-            int res = 0;
+            inputStream = new FileInputStream(fileName);
+            int commaCount = 0;
+
             if (inputStream.available() > 0) {
-                byte[] buffer = new byte[inputStream.available()];
+                buffer = new byte[inputStream.available()];
                 int count = inputStream.read(buffer);
-
-                for (int i = 0; i < buffer.length; i++) {
-                    if (buffer[i] == 44) {
-                        res++;
-                    }
-                }
             }
-            System.out.println(res);
-            inputStream.close();
 
+            for (int i = 0; i < buffer.length; i++) {
+                if (buffer[i] == 44) commaCount++;
+            }
 
+            System.out.println(commaCount);
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                reader.close();
+                inputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
+
 }
