17063996c0cd5bf9f3485947b8d32f5696c1fd2e
SHTUNDER <shtunder.andrey1yandex.by>
2020-11-14 22:30:27 +0300

    task 1815

---
+++ 2.JavaCore/src/com/codegym/task/task18/task1815/Solution.java
@@ -0,0 +1,45 @@
+package com.codegym.task.task18.task1815;
+
+import java.util.List;
+
+/* 
+Table
+
+*/
+
+public class Solution {
+    public class TableInterfaceWrapper implements TableInterface {
+        TableInterface tableInterface;
+
+        public TableInterfaceWrapper(TableInterface tableInterface) {
+            this.tableInterface = tableInterface;
+        }
+
+        @Override
+        public void setModel(List rows) {
+            System.out.println(rows.size());
+            tableInterface.setModel(rows);
+        }
+
+        @Override
+        public String getHeaderText() {
+            return tableInterface.getHeaderText().toUpperCase();
+        }
+
+        @Override
+        public void setHeaderText(String newHeaderText) {
+            tableInterface.setHeaderText(newHeaderText);
+        }
+    }
+
+    public interface TableInterface {
+        void setModel(List rows);
+
+        String getHeaderText();
+
+        void setHeaderText(String newHeaderText);
+    }
+
+    public static void main(String[] args) {
+    }
+}
