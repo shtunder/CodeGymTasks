19e3b2fbc49824fe33397816dea5fd396ca28fab
SHTUNDER <shtunder.andrey1yandex.by>
2020-11-19 00:27:47 +0300

    task 1819

--- 2.JavaCore/src/com/codegym/task/task18/task1819/Solution.java
+++ 2.JavaCore/src/com/codegym/task/task18/task1819/Solution.java
@@ -1,27 +1,31 @@
 package com.codegym.task.task18.task1819;
 
+import java.io.*;
+
 /* 
 Combining files
 
 */
 
-import java.io.*;
-
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
         String fileName1 = reader.readLine();
         String fileName2 = reader.readLine();
 
-        try (FileInputStream fio2 = new FileInputStream(fileName2);
-             FileInputStream fio1 = new FileInputStream(fileName1);
-             FileOutputStream foo1 = new FileOutputStream(fileName1)) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        try (FileInputStream fileInputStream1 = new FileInputStream(fileName1);
+             FileInputStream fileInputStream2 = new FileInputStream(fileName2)) {
 
-            while (fio2.available() > 0) {
-                
+            while (fileInputStream2.available() > 0) {
+                byteArrayOutputStream.write(fileInputStream2.read());
+            }
+            while (fileInputStream1.available() > 0) {
+                byteArrayOutputStream.write(fileInputStream1.read());
             }
         }
-
+        try (FileOutputStream fileOutputStream = new FileOutputStream(fileName1)) {
+            byteArrayOutputStream.writeTo(fileOutputStream);
+        }
     }
 }
