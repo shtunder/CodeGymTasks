02459f5c60e204e82ab195214b738f8a84b7ec02
SHTUNDER <shtunder.andrey1yandex.by>
2020-11-23 23:53:02 +0300

    task 1821

--- 2.JavaCore/src/com/codegym/task/task18/task1821/Solution.java
+++ 2.JavaCore/src/com/codegym/task/task18/task1821/Solution.java
@@ -1,12 +1,25 @@
 package com.codegym.task.task18.task1821;
 
+import java.io.FileReader;
+import java.io.IOException;
+
 /* 
 Symbol frequency
 
 */
 
 public class Solution {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        int[] aSCII = new int[128];
+        try (FileReader reader = new FileReader(args[0])) {
+            while (reader.ready()) {
+                aSCII[reader.read()]++;
+            }
+        }
+        for (int i = 0; i < aSCII.length; i++) {
+            if (aSCII[i] != 0) {
+                System.out.println((char) i + " " + aSCII[i]);
+            }
+        }
     }
 }
