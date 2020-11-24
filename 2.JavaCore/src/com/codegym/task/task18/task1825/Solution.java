0892b282d15e710526e5d66350027879b49b1d3b
SHTUNDER <shtunder.andrey1yandex.by>
2020-11-24 23:58:49 +0300

    task 1825

---
+++ 2.JavaCore/src/com/codegym/task/task18/task1825/Solution.java
@@ -0,0 +1,20 @@
+package com.codegym.task.task18.task1825;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
+
+/* 
+Building a file
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+}
