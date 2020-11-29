87c5b04938b230ddf93469dacea56e8bbc19574d
SHTUNDER <shtunder.andrey1yandex.by>
2020-11-29 23:53:19 +0300

    task 1824

--- 2.JavaCore/src/com/codegym/task/task18/task1824/Solution.java
+++ 2.JavaCore/src/com/codegym/task/task18/task1824/Solution.java
@@ -5,7 +5,20 @@ Files and exceptions
 
 */
 
+import java.io.*;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            String fileName = reader.readLine();
+            try (FileInputStream fileInputStream = new FileInputStream(fileName)) {
+
+            } catch (FileNotFoundException e) {
+                System.out.println(fileName);
+                break;
+            }
+        }
     }
 }
