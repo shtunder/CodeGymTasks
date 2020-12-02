43bba1c60e71c79565e6a8546114754f56163bf3
SHTUNDER <shtunder.andrey1yandex.by>
2020-12-03 00:13:30 +0300

    task 1826

--- 2.JavaCore/src/com/codegym/task/task18/task1826/Solution.java
+++ 2.JavaCore/src/com/codegym/task/task18/task1826/Solution.java
@@ -11,23 +11,17 @@ import java.io.IOException;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
+        String mode = args[0];
+        if (!(mode.equals("-e") || mode.equals("-d"))) return;
         try (FileInputStream fileInputStream = new FileInputStream(args[1]);
              FileOutputStream fileOutputStream = new FileOutputStream(args[2])) {
-            if (args[0].equals("-e")) {
-                while (fileInputStream.available() > 0) {
-                    int readByte = fileInputStream.read();
-                    int changedByte = readByte + 15;
-                    fileOutputStream.write(changedByte);
-                }
-            } else if (args[0].equals("-d")) {
-                while (fileInputStream.available() > 0) {
-                    int readByte = fileInputStream.read();
-                    int changedByte = readByte - 15;
-                    fileOutputStream.write(changedByte);
-                }
+
+            while (fileInputStream.available() > 0) {
+                int readByte = fileInputStream.read();
+                int changedByte = mode.equals("-e") ? readByte + 32 : readByte - 32;
+                fileOutputStream.write(changedByte);
             }
+
         }
-
     }
-
 }
