8be3b969d9654ec9f8f66ab60b51ac06cb4e8313
SHTUNDER <shtunder.andrey1yandex.by>
2020-12-19 23:29:52 +0300

    Reader wrapper

---
+++ 2.JavaCore/src/com/codegym/task/task19/task1911/Solution.java
@@ -0,0 +1,20 @@
+package com.codegym.task.task19.task1911;
+
+/* 
+Reader wrapper
+
+*/
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("This is text for testing");
+        }
+    }
+}
