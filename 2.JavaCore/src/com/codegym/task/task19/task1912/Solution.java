585c29a090394c5b3b6b6d9a7dc443ca9e825cd1
SHTUNDER <shtunder.andrey1yandex.by>
2020-12-23 23:48:55 +0300

    Reader wrapper 2

--- 2.JavaCore/src/com/codegym/task/task19/task1912/Solution.java
+++ 2.JavaCore/src/com/codegym/task/task19/task1912/Solution.java
@@ -1,5 +1,8 @@
 package com.codegym.task.task19.task1912;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 /* 
 Reader Wrapper 2
 
@@ -9,6 +12,15 @@ public class Solution {
     public static TestString testString = new TestString();
 
     public static void main(String[] args) {
+        PrintStream defaultPrintStream = System.out;
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(byteArrayOutputStream));
+        testString.printSomething();
+        System.setOut(defaultPrintStream);
+
+        String result = byteArrayOutputStream.toString();
+        result = result.replaceAll("te", "??");
+        System.out.println(result);
     }
 
     public static class TestString {
