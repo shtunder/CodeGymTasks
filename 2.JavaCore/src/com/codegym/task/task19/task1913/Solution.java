a64eb98f8c85d2d04c9dcc9a39af43bf8aab5e93
SHTUNDER <shtunder.andrey1yandex.by>
2020-12-24 23:52:02 +0300

    Output only digits

---
+++ 2.JavaCore/src/com/codegym/task/task19/task1913/Solution.java
@@ -0,0 +1,19 @@
+package com.codegym.task.task19.task1913;
+
+/* 
+Output only digits
+
+*/
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("This 1 text 23 is 4 f5-6or7 tes8ting");
+        }
+    }
+}
