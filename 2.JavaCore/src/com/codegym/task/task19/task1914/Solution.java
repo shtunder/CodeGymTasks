67bd055eada8c0e7e9849c67f83c1ab780198367
SHTUNDER <shtunder.andrey1yandex.by>
2020-12-27 22:52:59 +0300

    Problem solving

---
+++ 2.JavaCore/src/com/codegym/task/task19/task1914/Solution.java
@@ -0,0 +1,20 @@
+package com.codegym.task.task19.task1914;
+
+/* 
+Problem solving
+
+*/
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("3 + 6 = ");
+        }
+    }
+}
+
