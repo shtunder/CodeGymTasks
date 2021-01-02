99aab068baf8ee768f326e1f5b87e3dd3a383b95
SHTUNDER <shtunder.andrey1yandex.by>
2021-01-02 23:59:06 +0300

    Tracking changes

--- 2.JavaCore/src/com/codegym/task/task19/task1916/Solution.java
+++ 2.JavaCore/src/com/codegym/task/task19/task1916/Solution.java
@@ -1,5 +1,9 @@
 package com.codegym.task.task19.task1916;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,9 +13,54 @@ Tracking changes
 */
 
 public class Solution {
-    public static List<LineItem> lines = new ArrayList<>();
+    public static List<LineItem> lines = new ArrayList<LineItem>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+        String oldFileName = console.readLine();
+        String newFileName = console.readLine();
+        console.close();
+
+        List<String> oldFileLines = readFileLines(oldFileName);
+        List<String> newFileLines = readFileLines(newFileName);
+
+        int oldFileLine = 0;
+        int newFileLine = 0;
+
+        while ((oldFileLine < oldFileLines.size()) && (newFileLine < newFileLines.size())) {
+
+            if (oldFileLines.get(oldFileLine).equals(newFileLines.get(newFileLine))) {
+                lines.add(new LineItem(Type.SAME, oldFileLines.get(oldFileLine)));
+                oldFileLine++;
+                newFileLine++;
+            } else if ((newFileLine + 1 < newFileLines.size()) && oldFileLines.get(oldFileLine).equals(newFileLines.get(newFileLine + 1))) {
+                lines.add(new LineItem(Type.ADDED, newFileLines.get(newFileLine)));
+                newFileLine++;
+            } else if ((oldFileLine + 1 < oldFileLines.size()) && oldFileLines.get(oldFileLine + 1).equals(newFileLines.get(newFileLine))) {
+                lines.add(new LineItem(Type.REMOVED, oldFileLines.get(oldFileLine)));
+                oldFileLine++;
+            }
+        }
+
+        while (oldFileLine < (oldFileLines.size())) {
+            lines.add(new LineItem(Type.REMOVED, oldFileLines.get(oldFileLine)));
+            oldFileLine++;
+        }
+        while (newFileLine < (newFileLines.size())) {
+            lines.add(new LineItem(Type.ADDED, newFileLines.get(newFileLine)));
+            newFileLine++;
+        }
+    }
+
+    static List<String> readFileLines(String fileName) throws IOException {
+        BufferedReader fReader = new BufferedReader(new FileReader(fileName));
+        List<String> fileLines = new ArrayList<String>();
+        String line;
+        while ((line = fReader.readLine()) != null) {
+            fileLines.add(line);
+        }
+        fReader.close();
+        return fileLines;
     }
 
     public static enum Type {
