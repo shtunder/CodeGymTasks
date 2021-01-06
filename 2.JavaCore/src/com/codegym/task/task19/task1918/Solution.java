fb9a59fbb6e32bd1b597940ba6344a13007c288c
SHTUNDER <shtunder.andrey1yandex.by>
2021-01-06 23:39:49 +0300

    Introducing tags

--- 2.JavaCore/src/com/codegym/task/task19/task1918/Solution.java
+++ 2.JavaCore/src/com/codegym/task/task19/task1918/Solution.java
@@ -1,5 +1,12 @@
 package com.codegym.task.task19.task1918;
 
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 /* 
 Introducing tags
 
@@ -7,5 +14,54 @@ Introducing tags
 
 public class Solution {
     public static void main(String[] args) {
+
+        String fileName = null;
+
+        try (BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in))) {
+            fileName = consoleReader.readLine();
+        } catch (IOException ignore) {
+            /*NOP*/
+        }
+
+        StringBuilder readFileContent = new StringBuilder();
+
+        try (BufferedReader fileReader = new BufferedReader(new FileReader(fileName))) {
+            while (fileReader.ready()) {
+                readFileContent = readFileContent.append(fileReader.readLine());
+            }
+        } catch (IOException ignore) {
+            /*NOP*/
+        }
+
+        String fileContent = readFileContent.toString().replaceAll("\r\n", "");
+
+        String openTag = "<" + args[0];
+        String closingTag = "</" + args[0];
+        int tagLength = args[0].length();
+        int startTagIndex = 0;
+        int endTagIndex = 0;
+
+        ArrayList<String> tags = new ArrayList<>();
+
+        while ((startTagIndex != -1) && (startTagIndex < fileContent.length())) {
+            startTagIndex = fileContent.indexOf(openTag, startTagIndex);
+            endTagIndex = fileContent.indexOf(closingTag, startTagIndex + tagLength);
+
+            int indexInTag = startTagIndex + tagLength;
+            if (endTagIndex != -1) {
+                while (fileContent.substring(indexInTag, endTagIndex).contains(openTag)) {
+                    indexInTag = endTagIndex + tagLength;
+                    endTagIndex = fileContent.indexOf(closingTag, indexInTag);
+                }
+            }
+            if (startTagIndex != -1 && endTagIndex != -1) {
+                tags.add(fileContent.substring(startTagIndex, endTagIndex + tagLength + 3));
+                startTagIndex += tagLength;
+            }
+        }
+
+        for (String tag : tags) {
+            System.out.println(tag);
+        }
     }
 }
