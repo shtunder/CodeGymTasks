85cb4145b5aa9ba63707510a19c50e8d2909e3f8
SHTUNDER <shtunder.andrey1yandex.by>
2021-01-11 22:11:34 +0300

    John Johnson

---
+++ 2.JavaCore/src/com/codegym/task/task19/task1921/Solution.java
@@ -0,0 +1,17 @@
+package com.codegym.task.task19.task1921;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+John Johnson
+
+*/
+
+public class Solution {
+    public static final List<Person> PEOPLE = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+    }
+}
