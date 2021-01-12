02a4a204cc7245a56df49c4ada9c003fe313c142
SHTUNDER <shtunder.andrey1yandex.by>
2021-01-12 23:50:43 +0300

    John Johnson

--- 2.JavaCore/src/com/codegym/task/task19/task1921/Solution.java
+++ 2.JavaCore/src/com/codegym/task/task19/task1921/Solution.java
@@ -12,6 +12,13 @@ public class Solution {
     public static final List<Person> PEOPLE = new ArrayList<>();
 
     public static void main(String[] args) {
+        String fileName = args[0];
 
+        ArrayList<String> fileContent = new ArrayList<>();
+        try (BufferedReader fileReader = new BufferedReader(new FileReader(fileName))) {
+            String fileLine;
+            while ((fileLine = fileReader.readLine()) != null)
+                fileContent.add(fileLine);
+
+        }
     }
-}
