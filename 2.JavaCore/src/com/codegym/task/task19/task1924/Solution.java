52f474a81255a59e01f1d427f1309e98ed9b43cc
SHTUNDER <shtunder.andrey1yandex.by>
2021-01-19 22:57:11 +0300

    Replacing numbers

---
+++ 2.JavaCore/src/com/codegym/task/task19/task1924/Solution.java
@@ -0,0 +1,17 @@
+package com.codegym.task.task19.task1924;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/* 
+Replacing numbers
+
+*/
+
+public class Solution {
+    public static Map<Integer, String> map = new HashMap<Integer, String>();
+
+    public static void main(String[] args) {
+
+    }
+}
