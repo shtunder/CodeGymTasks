9d9675f92e9607659644be957cdd9dec53f9bed7
SHTUNDER <shtunder.andrey1yandex.by>
2021-01-20 23:18:21 +0300

    Replacing numbers

--- 2.JavaCore/src/com/codegym/task/task19/task1924/Solution.java
+++ 2.JavaCore/src/com/codegym/task/task19/task1924/Solution.java
@@ -1,5 +1,10 @@
 package com.codegym.task.task19.task1924;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,7 +14,23 @@ Replacing numbers
 */
 
 public class Solution {
-    public static Map<Integer, String> map = new HashMap<Integer, String>();
+    public static Map<Integer, String> map = new HashMap<>();
+
+    static {
+        map.put(12, "twelve");
+        map.put(11, "eleven");
+        map.put(10, "ten");
+        map.put(9, "nine");
+        map.put(8, "eight");
+        map.put(7, "seven");
+        map.put(6, "six");
+        map.put(5, "five");
+        map.put(4, "four");
+        map.put(3, "three");
+        map.put(2, "two");
+        map.put(1, "one");
+        map.put(0, "zero");
+    }
 
     public static void main(String[] args) {
 
