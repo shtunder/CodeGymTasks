b5282daed9d338c8b9153fdd22ca2b6c8fa906ba
SHTUNDER <shtunder.andrey1yandex.by>
2021-01-27 23:39:55 +0300

    Mirror image

--- 2.JavaCore/src/com/codegym/task/task19/task1926/Solution.java
+++ 2.JavaCore/src/com/codegym/task/task19/task1926/Solution.java
@@ -1,5 +1,10 @@
 package com.codegym.task.task19.task1926;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /* 
 Mirror image
 
@@ -7,5 +12,14 @@ Mirror image
 
 public class Solution {
     public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             BufferedReader fileIn = new BufferedReader(new FileReader(reader.readLine()))) {
+            while (fileIn.ready()) {
+                String line = fileIn.readLine();
+                System.out.println(new StringBuffer(line).reverse().toString());
+            }
+        } catch (IOException ignore) {
+            /* NOP */
+        }
     }
 }
