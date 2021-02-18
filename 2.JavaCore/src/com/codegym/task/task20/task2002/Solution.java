8a27c61738c33dafe7ff9859e7b8f25157971880
SHTUNDER <shtunder.andrey1yandex.by>
2021-02-18 23:53:50 +0300

    Reading and writing to a file: CodeGym

--- 2.JavaCore/src/com/codegym/task/task20/task2002/Solution.java
+++ 2.JavaCore/src/com/codegym/task/task20/task2002/Solution.java
@@ -2,6 +2,7 @@ package com.codegym.task.task20.task2002;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /* 
@@ -25,16 +26,17 @@ public class Solution {
             CodeGym loadedObject = new CodeGym();
             loadedObject.load(inputStream);
             // Here check that the codeGym object is equal to the loadedObject object
+            System.out.println(codeGym.equals(loadedObject));
 
             outputStream.close();
             inputStream.close();
 
         } catch (IOException e) {
             //e.printStackTrace();
-            System.out.println("Oops, something is wrong with my file");
+            System.out.println("Oops, something wrong with my file");
         } catch (Exception e) {
             //e.printStackTrace();
-            System.out.println("Oops, something is wrong with the save/load method");
+            System.out.println("Oops, something wrong with save/load method");
         }
     }
 
@@ -43,6 +45,24 @@ public class Solution {
 
         public void save(OutputStream outputStream) throws Exception {
             // Implement this method
+            PrintWriter printWriter = new PrintWriter(outputStream);
+            if (this.users.size() > 0) {
+                for (User current : this.users) {
+                    StringBuilder sb = new StringBuilder();
+                    sb.append(current.getFirstName());
+                    sb.append("/");
+                    sb.append(current.getLastName());
+                    sb.append("/");
+                    sb.append(current.getBirthDate().getTime());
+                    sb.append("/");
+                    sb.append(current.isMale());
+                    sb.append("/");
+                    sb.append(current.getCountry());
+
+                    printWriter.println(sb.toString());
+                }
+            }
+            printWriter.close();
         }
 
         public void load(InputStream inputStream) throws Exception {
