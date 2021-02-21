75010e485e5b63eaf2ea463bbe8ef25e9d2948c9
SHTUNDER <shtunder.andrey1yandex.by>
2021-02-21 23:58:15 +0300

    Introducing properties

--- 2.JavaCore/src/com/codegym/task/task20/task2003/Solution.java
+++ 2.JavaCore/src/com/codegym/task/task20/task2003/Solution.java
@@ -1,6 +1,9 @@
 package com.codegym.task.task20.task2003;
 
 import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
 /* 
 Introducing properties
@@ -10,8 +13,16 @@ Introducing properties
 public class Solution {
     public static Map<String, String> properties = new HashMap<>();
 
-    public void fillInPropertiesMap() {
+    public void fillInPropertiesMap() throws Exception {
         // Implement this method
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String filename = reader.readLine();
+
+        FileInputStream in = new FileInputStream(filename);
+
+        load(in);
+
+        reader.close();
     }
 
     public void save(OutputStream outputStream) throws Exception {
