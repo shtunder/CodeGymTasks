3f7a3780752aeb8874cf5c35c3a1fa3845561054
SHTUNDER <shtunder.andrey1yandex.by>
2021-03-02 23:48:44 +0300

    How do you serialize CodeGym?

---
+++ 2.JavaCore/src/com/codegym/task/task20/task2007/Solution.java
@@ -0,0 +1,19 @@
+package com.codegym.task.task20.task2007;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+How do you serialize CodeGym?
+
+*/
+
+public class Solution {
+    public static class CodeGym {
+        public List<User> users = new ArrayList<>();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
