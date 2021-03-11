dd6f8ee29b0d4be12c759863ad7bfeccd43dc9a5
SHTUNDER <shtunder.andrey1yandex.by>
2021-03-11 23:48:30 +0300

    Externalizable for apartments

--- 2.JavaCore/src/com/codegym/task/task20/task2011/Solution.java
+++ 2.JavaCore/src/com/codegym/task/task20/task2011/Solution.java
@@ -1,12 +1,18 @@
 package com.codegym.task.task20.task2011;
 
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+
 /* 
 Externalizable for apartments
+
 */
 
 public class Solution {
 
-    public static class Apartment {
+    public static class Apartment implements Externalizable {
 
         private String address;
         private int year;
@@ -21,6 +27,22 @@ public class Solution {
         public Apartment(String addr, int y) {
             address = addr;
             year = y;
+        }
+
+        /**
+         * Mandatory writeExernal method.
+         */
+        public void writeExternal(ObjectOutput out) throws IOException {
+            out.writeObject(address);
+            out.writeInt(year);
+        }
+
+        /**
+         * Mandatory readExternal method.
+         */
+        public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+            address = (String) in.readObject();
+            year = in.readInt();
         }
 
         /**
