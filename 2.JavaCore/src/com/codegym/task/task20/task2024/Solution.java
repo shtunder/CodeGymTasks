a4f9ad6ba6366f4f42e281e3e8f0adab02f5c456
SHTUNDER <shtunder.andrey1yandex.by>
2021-04-05 23:46:09 +0300

    Introducing graphs

---
+++ 2.JavaCore/src/com/codegym/task/task20/task2024/Solution.java
@@ -0,0 +1,18 @@
+package com.codegym.task.task20.task2024;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/* 
+Introducing graphs
+
+*/
+
+public class Solution {
+    int node;
+    List<Solution> edges = new LinkedList<>();
+
+    public static void main(String[] args) {
+
+    }
+}
