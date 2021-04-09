1304810dff52bff9d7bfc850349bc57af8e42492
SHTUNDER <shtunder.andrey1yandex.by>
2021-04-09 23:47:16 +0300

    Rectangle algorithms

---
+++ 2.JavaCore/src/com/codegym/task/task20/task2026/Solution.java
@@ -0,0 +1,32 @@
+package com.codegym.task.task20.task2026;
+
+/* 
+Rectangle algorithms
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        byte[][] a1 = new byte[][]{
+                {1, 1, 0, 0},
+                {1, 1, 0, 0},
+                {1, 1, 0, 0},
+                {1, 1, 0, 1}
+        };
+        byte[][] a2 = new byte[][]{
+                {1, 0, 0, 1},
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {1, 0, 0, 1}
+        };
+
+        int count1 = getRectangleCount(a1);
+        System.out.println("count = " + count1 + ". Must be 2");
+        int count2 = getRectangleCount(a2);
+        System.out.println("count = " + count2 + ". Must be 4");
+    }
+
+    public static int getRectangleCount(byte[][] a) {
+        return 0;
+    }
+}
