967ab2fde135665d4cbd8280f4db12677080ee87
SHTUNDER <shtunder.andrey1yandex.by>
2021-04-10 23:54:04 +0300

    Rectangle algorithms

--- 2.JavaCore/src/com/codegym/task/task20/task2026/Solution.java
+++ 2.JavaCore/src/com/codegym/task/task20/task2026/Solution.java
@@ -26,7 +26,68 @@ public class Solution {
         System.out.println("count = " + count2 + ". Must be 4");
     }
 
-    public static int getRectangleCount(byte[][] a) {
-        return 0;
+    public static int getRectangleCount(byte[][] arr) {
+        byte[][] a = new byte[arr.length][arr[0].length];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length; j++) {
+                a[i][j] = arr[i][j];
+            }
+        }
+
+        int hor = a[0].length;
+        int ver = a.length;
+        int count;
+        int verEnd;
+        int horEnd;
+        int result = 0;
+        for (int i = 0; i < ver; i++) {
+            for (int j = 0; j < hor; j++) {
+                if (a[i][j] == 1) {
+                    verEnd = i;
+                    horEnd = j;
+                    // vertically
+                    count = i + 1;
+                    System.out.println("a[" + count + "][" + j + "]");
+                    while (true) {
+                        if (count >= ver || (a[count][j] == 0 && count < ver)) {
+                            verEnd = count - 1;
+                            System.out.println("verEnd = " + verEnd);
+                            break;
+                        }
+                        count++;
+                        System.out.println("a[" + count + "][" + j + "]");
+                    }
+                    // horizontally
+                    count = j + 1;
+                    while (true) {
+                        if (count >= hor || (a[i][count] == 0 && count < hor)) {
+                            horEnd = count - 1;
+                            System.out.println("horEnd = " + horEnd);
+                            break;
+                        }
+                        count++;
+                    }
+                    boolean flag = true;
+                    for (int p = i; p <= verEnd; p++)
+                        for (int q = j; q <= horEnd; q++)
+                            if (a[p][q] != 1) {
+                                flag = false;
+                                break;
+                            }
+                    if (flag) {
+                        System.out.println("startI = " + i);
+                        System.out.println("startJ = " + j);
+                        System.out.println("endI = " + verEnd);
+                        System.out.println("endJ = " + horEnd);
+                        for (int p = i; p <= verEnd; p++)
+                            for (int q = j; q <= horEnd; q++)
+                                a[p][q] = 0;
+                        result++;
+                    }
+                    System.out.println();
+                }
+            }
+        }
+        return result;
     }
 }
